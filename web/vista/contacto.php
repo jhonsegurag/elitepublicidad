2cae72250f59c145ab63f7753f6c0ee8b07b703e
admont28 <admont28@gmail.com>
2014-02-19 11:09:49 -0500

    Google Maps
    Se agrega un mapa de google en la página contacto.php con un enlace
    hacia Google Street View.

--- web/vista/contacto.php
+++ web/vista/contacto.php
@@ -38,20 +38,27 @@
 								</div>
 							</div>
 							
+							<aside>
+								<div id="map">
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.3061963384243!2d-75.673798750
+									00003!3d4.538790650000008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38f5a97708046f%3A0xd9
+									f7d001201fc8d3!2sCarrera+21+%23+15-3!5e0!3m2!1ses-419!2s!4v1392822817407" 
+									 frameborder="0" style="border:0">
+									</iframe>
+								</div>
+								<div id="streetView">
+									<a href="https://www.google.com/maps/place/Carrera+21+%23+15-3/@4.53881,-75.67379,3a,90y,186h,90t/dat
+									a=!3m4!1e1!3m2!1sGqfN2nvxwwYX65UQj8A59Q!2e0!4m2!3m1!1s0x8e38f5a97708046f:0xd9f7d001201fc8d3!6m1!1e1">
+									Encuentranos en Google Street View</a>
+								</div>
+							</aside>
 							
 						</section>
 						<footer>
 							
 						</footer>
 					</article>
-					
 				</section>
-
-
-
-
-
-
 	<!-- --------------------CÓDIGO HTML------------------------------------------------ -->
 			<?php
 				getFooter();
