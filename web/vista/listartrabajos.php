db86419544419f6308f6b2e5de045f6e4b5f2dd3
admont28 <admont28@gmail.com>
2014-03-04 00:29:25 -0500

    Listar Trabajos realizados.
    Se agrega la imagen del trabajo realizado en la parte izquierda de la
    tabla cuando se listan todos los trabajos.

--- web/vista/listartrabajos.php
+++ web/vista/listartrabajos.php
@@ -26,9 +26,8 @@
 				<section>
 					<table  style="border-radius: 5px; border: 1px solid #222; width: 100%;">
 						<thead>
-							<th>Id</th>
+							<th>Imágen</th>
 							<th>Descripción</th>
-							<th>Ruta</th>
 							<th>Editar</th>
 							<th>Eliminar</th>
 						</thead>
@@ -39,14 +38,13 @@
 							<tr>
 								
 								<td >
-									<?php echo "".$trabajosrealizados[$i]['idtrabajorealizado']; ?>
+									<div id="imagenProducto" style="width: 50px; height: 50px; margin: auto;">
+										<img style="width: 100%; height: 100%;" src="<?php echo "".$trabajosrealizados[$i]['ruta']; ?>" />
+									</div>
 								</td> 
 								<td >
 									<?php echo "".$trabajosrealizados[$i]['descripcion']; ?>
 								</td> 
-								<td >
-									<?php echo "".$trabajosrealizados[$i]['ruta']; ?>
-								</td>
 								<td >
 									<a href="<?php echo "editarTrabajo?idtrabajo=".$trabajosrealizados[$i]['idtrabajorealizado']; ?>">Editar</a>
 								</td>
